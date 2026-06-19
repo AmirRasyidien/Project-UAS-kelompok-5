0348c135aca7f19612953f2352091083507b7a32
SILMI30 <2503010086@unper.ac.id>
2026-06-19 14:57:02 +0700

    Add files via upload

---
+++ silmi_satu.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namwspace std;
+
+// struktur data pasien
+struct pasien{
+	int id;
+	string nama;
+	int umur;
+	string alamat;
+	string keluhan
+};
+
+// node linked list pasien
+struct node{
+	pasien data;
+	node* next;
+};
+
+node* head = NULL;
+
+// tambah pasien
+void tambahPasien(){
+	node* baru = new node;
+
+	cout << "\n=====Tambah Data Pasien=====\n";
+	cout << "ID Pasien :" ;
+	cin >> baru->data.id;
+	cin.igore();
+
+	cout << "Nama      :";
+	getline(cin, baru->data.nama;
+
+	cout << "Umur      :";
+	cin >> baru->data.umur;
+	cin.ignore();
+
+	cout << "Alamat    :";
+	getline(cin, baru->data.alamat);
+
+	cout << "Keluhan   :";
+	getline(cin, baru->data.keluhan);
+
+}
+
+int main() {
+	tambahPasien();
+}
